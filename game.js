41e43f4f1a0fa49645a428e0c7f29e3aeb6f2295
tyraedin <edty24pr@student.ju.se>
2024-11-15 14:22:55 +0100

    Update game.js
    save

--- game.js
+++ game.js
@@ -1,6 +1,146 @@
 x = 200;
 y = 200;
 
+let state = "start";
+let gameTimer = 0;
+
+function startScreen() {
+  background(0, 0, 0);
+  text("start", 200, 200);
+}
+function resultScreen() {
+  background(0, 0, 0);
+  text("result", 200, 200);
+}
+function gameScreen() {
+  background(25, 14, 20);
+  push();
+  noStroke();
+
+  for (let index in starx) {
+    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
+    ellipse(starx[index], stary[index], 3);
+    starAlpha[index] = starAlpha[index] + 0.05;
+  }
+  // rooftop
+  fill(200, 200, 200);
+  rect(x - 200, y + 200, 600, 200);
+  fill(100, 100, 100);
+  rect(x - 200, y + 150, 600, 50);
+
+  //1 skyscraper
+  fill(70, 70, 70);
+  rect(x - 200, y - 50, 100, 200);
+  fill(255, 255, 200);
+  rect(x - 200, y - 15, 90, 10);
+  rect(x - 200, y + 15, 90, 10);
+  rect(x - 200, y + 45, 90, 10);
+  rect(x - 200, y + 75, 90, 10);
+  rect(x - 200, y + 105, 90, 10);
+  rect(x - 200, y + 135, 90, 10);
+
+  //skyscraper 3
+  fill(85, 50, 75);
+  rect(x - 60, y - 90, 110, 240);
+  rect(x - 45, y - 110, 80, 20);
+  rect(x - 25, y - 130, 40, 20);
+  rect(x - 10, y - 155, 10, 25);
+
+  //2 skyscraper
+  fill(70, 70, 100);
+  rect(x - 120, y + 25, 90, 125);
+  fill(x + 55, y + 55, 200);
+  rect(x - 110, y + 50, 10, 10);
+  rect(x - 90, y + 50, 10, 10);
+  rect(x - 70, y + 50, 10, 10);
+  rect(x - 50, y + 50, 10, 10);
+  rect(x - 90, y + 50, 10, 10);
+  rect(x - 110, y + 70, 10, 10);
+  rect(x - 90, y + 70, 10, 10);
+  rect(x - 70, y + 70, 10, 10);
+  rect(x - 50, y + 70, 10, 10);
+  rect(x - 90, y + 70, 10, 10);
+  rect(x - 110, y + 90, 10, 10);
+  rect(x - 90, y + 90, 10, 10);
+  rect(x - 70, y + 90, 10, 10);
+  rect(x - 50, y + 90, 10, 10);
+  rect(x - 90, y + 90, 10, 10);
+  rect(x - 110, y + 110, 10, 10);
+  rect(x - 90, y + 110, 10, 10);
+  rect(x - 70, y + 110, 10, 10);
+  rect(x - 50, y + 110, 10, 10);
+  rect(x - 90, y + 110, 10, 10);
+  rect(x - 110, y + 130, 10, 10);
+  rect(x - 90, y + 130, 10, 10);
+  rect(x - 70, y + 130, 10, 10);
+  rect(x - 50, y + 130, 10, 10);
+  rect(x - 90, y + 130, 10, 10);
+
+  //skyscraper 4
+  fill(90, 95, 80);
+  rect(x + 40, y - 20, 70, 170);
+  fill(255, 255, 200);
+  rect(x + 50, y + 10, 50, 5);
+  rect(x + 50, y + 30, 50, 5);
+  rect(x + 50, y + 50, 50, 5);
+  rect(x + 50, y + 70, 50, 5);
+  rect(x + 50, y + 90, 50, 5);
+  rect(x + 50, y + 110, 50, 5);
+  rect(x + 50, y + 130, 50, 5);
+
+  //skyscraper 5
+  fill(70, 60, 70);
+  rect(x + 110, y - 50, 80, 200);
+  fill(255, 255, 200);
+  rect(x + 125, y - 20, 20, 20);
+  rect(x + 155, y - 20, 20, 20);
+  rect(x + 125, y + 10, 20, 20);
+  rect(x + 155, y + 10, 20, 20);
+  rect(x + 125, y + 40, 20, 20);
+  rect(x + 155, y + 40, 20, 20);
+  rect(x + 125, y + 70, 20, 20);
+  rect(x + 155, y + 70, 20, 20);
+
+  //moon
+  push();
+  translate(x - 140, y - 130);
+  rotate(1.5);
+  arc(0, 0, 50, 50, 0, PI);
+  fill(25, 14, 20);
+  ellipse(x - 200, y - 205, 50, 40);
+  pop();
+  fill(100, 100, 100);
+  triangle(x - 200, y + 159, x - 160, y + 150, x - 200, y + 370);
+  triangle(x + 384, y + 150, x + 340, y + 150, x + 385, y + 370);
+
+  //skyscraper 7
+  fill(30, 50, 80);
+  rect(x + 260, y - 80, 100, 230);
+  push();
+  fill(150,150,150);
+  arc(x+310,y-80,100,70,PI,0);
+
+  line(x+305,y-170,x+305,y-20;
+pop();
+  //skyscraper 6
+  fill(60, 18, 80);
+  rect(x + 180, y + 40, 100, 110);
+  fill(255, 255, 200);
+  rect(x + 200, y + 60, 20, 10);
+  rect(x + 240, y + 60, 20, 10);
+  rect(x + 200, y + 80, 20, 10);
+  rect(x + 240, y + 80, 20, 10);
+  rect(x + 200, y + 100, 20, 10);
+  rect(x + 240, y + 100, 20, 10);
+  rect(x + 200, y + 120, 20, 10);
+  rect(x + 240, y + 120, 20, 10);
+  rect(x + 200, y + 140, 20, 10);
+  rect(x + 240, y + 140, 20, 10);
+
+  pop();
+
+  character(x + 70, y + 170);
+}
 //* Code taken from Garrits video "Create a night sky"* //
 let starx = [];
 let stary = [];
@@ -18,7 +158,7 @@ for (let i = 0; i < 400; i++) {
 }
 
 function setup() {
-  createCanvas(585, 600);
+  createCanvas(550, 600);
 }
 
 function character(x, y) {
@@ -156,109 +296,33 @@ function hair(x, y) {
   bezierVertex(x - 43, y + 29, x - 9, y + 28, x - 26, y);
   endShape();
 }
+gameScreen(x, y);
+character(x + 70, y + 170);
 
 function draw() {
-  background(25, 14, 20);
-  push();
-  noStroke();
+  gameScreen(x, y);
+  character(x + 70, y + 170);
+}
 
-  for (let index in starx) {
-    fill(255, 255, 255, Math.abs(Math.sin(starAlpha[index])) * 255);
-    ellipse(starx[index], stary[index], 3);
-    starAlpha[index] = starAlpha[index] + 0.05;
+function mouseClicked() {
+  if (state === "start") {
+    state = "game";
+  } else if (state === "game") {
+    state = "result";
+  } else if (state === "result") {
+    state = "game";
   }
-  // rooftop
-  fill(200, 200, 200);
-  rect(x - 200, y + 200, 600, 200);
-  fill(100, 100, 100);
-  rect(x - 200, y + 150, 600, 50);
+}
 
-  //1 skyscraper
-  fill(70, 70, 70);
-  rect(x - 200, y - 50, 100, 200);
-  fill(255, 255, 200);
-  rect(x - 200, y - 15, 90, 10);
-  rect(x - 200, y + 15, 90, 10);
-  rect(x - 200, y + 45, 90, 10);
-  rect(x - 200, y + 75, 90, 10);
-  rect(x - 200, y + 105, 90, 10);
-  rect(x - 200, y + 135, 90, 10);
-
-  //skyscraper 3
-  fill(85, 50, 75);
-  rect(x - 60, y - 90, 110, 240);
-  rect(x - 45, y - 110, 80, 20);
-  rect(x - 25, y - 130, 40, 20);
-  rect(x - 10, y - 155, 10, 25);
-
-  //2 skyscraper
-  fill(70, 70, 100);
-  rect(x - 120, y + 25, 90, 125);
-  fill(x + 55, y + 55, 200);
-  rect(x - 110, y + 50, 10, 10);
-  rect(x - 90, y + 50, 10, 10);
-  rect(x - 70, y + 50, 10, 10);
-  rect(x - 50, y + 50, 10, 10);
-  rect(x - 90, y + 50, 10, 10);
-  rect(x - 110, y + 70, 10, 10);
-  rect(x - 90, y + 70, 10, 10);
-  rect(x - 70, y + 70, 10, 10);
-  rect(x - 50, y + 70, 10, 10);
-  rect(x - 90, y + 70, 10, 10);
-  rect(x - 110, y + 90, 10, 10);
-  rect(x - 90, y + 90, 10, 10);
-  rect(x - 70, y + 90, 10, 10);
-  rect(x - 50, y + 90, 10, 10);
-  rect(x - 90, y + 90, 10, 10);
-  rect(x - 110, y + 110, 10, 10);
-  rect(x - 90, y + 110, 10, 10);
-  rect(x - 70, y + 110, 10, 10);
-  rect(x - 50, y + 110, 10, 10);
-  rect(x - 90, y + 110, 10, 10);
-  rect(x - 110, y + 130, 10, 10);
-  rect(x - 90, y + 130, 10, 10);
-  rect(x - 70, y + 130, 10, 10);
-  rect(x - 50, y + 130, 10, 10);
-  rect(x - 90, y + 130, 10, 10);
-
-  //skyscraper 4
-  fill(90, 95, 80);
-  rect(x + 40, y - 20, 70, 170);
-  fill(255, 255, 200);
-  rect(x + 50, y + 10, 50, 5);
-  rect(x + 50, y + 30, 50, 5);
-  rect(x + 50, y + 50, 50, 5);
-  rect(x + 50, y + 70, 50, 5);
-  rect(x + 50, y + 90, 50, 5);
-  rect(x + 50, y + 110, 50, 5);
-  rect(x + 50, y + 130, 50, 5);
-
-  //skyscraper 5
-  fill(70, 60, 70);
-  rect(x + 110, y - 50, 80, 200);
-  fill(255, 255, 200);
-  rect(x + 125, y - 20, 20, 20);
-  rect(x + 155, y - 20, 20, 20);
-  rect(x + 125, y + 10, 20, 20);
-  rect(x + 155, y + 10, 20, 20);
-  rect(x + 125, y + 40, 20, 20);
-  rect(x + 155, y + 40, 20, 20);
-  rect(x + 125, y + 70, 20, 20);
-  rect(x + 155, y + 70, 20, 20);
-
-  //moon
-  push();
-  translate(x - 140, y - 130);
-  rotate(1.5);
-  arc(0, 0, 50, 50, 0, PI);
-  fill(25, 14, 20);
-  ellipse(x - 200, y - 205, 50, 40);
-  pop();
-  fill(100, 100, 100);
-  triangle(x - 200, y + 159, x - 160, y + 150, x - 200, y + 370);
-  triangle(x + 384, y + 150, x + 340, y + 150, x + 385, y + 370);
-  pop();
-
-  //ted
-  character(x + 50, y + 160);
+if (state === "start") {
+  startScreen();
+} else if (state === "game") {
+  gameScreen();
+  gameTimer = gameTimer + 1;
+  if (gameTimer >= 100) {
+    gameTimer = 0;
+    state = "result";
+  }
+} else if (state === "result") {
+  resultScreen();
 }
